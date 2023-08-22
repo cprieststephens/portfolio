86b66ec8252cf94d4e611b1ef02ddd1bfcd0c0f1
Camilla Prist-Stephens <cprieststephens@googlemail.com>
2023-08-22 13:15:04 +0100

    Reorganise project folders

---
+++ src/tests/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from '../components/App';
+
+test('renders correct title', () => {
+  render(<App />);
+  const portfolioTitle = screen.getByText(/portfolio/i);
+  expect(portfolioTitle ).toBeInTheDocument();
+});
