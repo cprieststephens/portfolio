ed7a606689e9ecf03cb7e880eae9917f0af49a2f
Camilla Prist-Stephens <cprieststephens@googlemail.com>
2023-08-23 14:04:13 +0100

    Add content to About component and style)

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "../styles/about.css";
 
 const About = () => {
-  return <h2>About</h2>;
+  return (
+    <div className="about">
+      <h1 className="about__name">Camilla Priest-Stephens</h1>
+      <h2 className="about__job-title">Frontend Developer</h2>
+      <p className="about__tech">
+        React, JavaScript, CSS, Jest, React Testing Library
+      </p>
+    </div>
+  );
 };
 
 export default About;
