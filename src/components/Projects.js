58bb185cd8f6b7482b7b0eaf08cff8680387fbff
Camilla Prist-Stephens <cprieststephens@googlemail.com>
2023-08-25 07:06:49 +0100

    Add third project to Projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import cocktailCompanion from "../img/cocktail-companion.jpg";
+import surrealEstate from "../img/surreal-estate.jpg";
 import weatherApp from "../img/weather-app.jpg";
 import "../styles/projects.css";
 
@@ -9,7 +10,7 @@ const Projects = () => {
     <div className="projects">
       <div className="projects-item">
         <h1>Cocktail Companion</h1>
-        <Link to={process.env.REACT_APP_COCKTAIL_SITE} target="_blank">
+        <Link to="https://www.cocktailcompanion.bar/" target="_blank">
           <img
             className="projects-item__image"
             src={cocktailCompanion}
@@ -17,18 +18,44 @@ const Projects = () => {
           />
         </Link>
         <p>
-          This collaboratively built app fetches and displays cocktail data from
-          an API. It is built with React, JavaScript, CSS, Jest and React
+          Collaboratively built with React, JavaScript, CSS, Jest and React
           Testing Library.
         </p>
         <p>
-          Users can search by name, filter by spirit and click on any cocktail
-          to view ingredients and instructions.
+          This cocktail mixing app displays API data and allows users to search
+          for drinks by name, filter by spirit and click on cocktail cards to
+          view ingredients and instructions.
         </p>
         <p>
           <Link
             className="projects-item__repo"
-            to={process.env.REACT_APP_COCKTAIL_REPO}
+            to="https://github.com/josenymad/cocktail-companion"
+            target="_blank"
+          >
+            View the Github repo here.
+          </Link>
+        </p>
+      </div>
+      <div className="projects-item">
+        <h1>Surreal Estate</h1>
+        <img
+          className="projects-item__image"
+          src={surrealEstate}
+          alt="Surreal Estate screenshot"
+        />
+        <p>
+          Built with React, JavaScript, CSS, Firebase, Jest and React Testing
+          Library.
+        </p>
+        <p>
+          This property listing app displays API data and allows users to search
+          by keyword, filter by city and sort by price. This project is
+          currently in progress.
+        </p>
+        <p>
+          <Link
+            className="projects-item__repo"
+            to="https://github.com/cprieststephens/surreal-estate"
             target="_blank"
           >
             View the Github repo here.
@@ -37,7 +64,7 @@ const Projects = () => {
       </div>
       <div className="projects-item">
         <h1>Weather App</h1>
-        <Link to={process.env.REACT_APP_WEATHER_SITE} target="_blank">
+        <Link to="https://check-the-forecast.netlify.app/" target="_blank">
           <img
             className="projects-item__image"
             src={weatherApp}
@@ -45,17 +72,17 @@ const Projects = () => {
           />
         </Link>
         <p>
-          This app fetches and displays weather data from an API. It is built
-          with React, JavaScript, CSS, Jest and React Testing Library.
+          Built with React, JavaScript, CSS, Jest and React Testing Library.
         </p>
         <p>
-          Users can search by city to view a five day forecast. They can also
-          access a more detailed forecast for each date.
+          This weather app allows users to search by city to fetch forecast data
+          from an API. Users can also access a more detailed forecast for each
+          date by clicking a button.
         </p>
         <p>
           <Link
             className="projects-item__repo"
-            to={process.env.REACT_APP_WEATHER_REPO}
+            to="https://github.com/cprieststephens/weather-app"
             target="_blank"
           >
             View the Github repo here.
