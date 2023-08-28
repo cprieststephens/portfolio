7fb9149bf03bd4849295fca51b51d23c19916ea2
Camilla Prist-Stephens <cprieststephens@googlemail.com>
2023-08-28 15:06:33 +0100

    Add tests and snapshot for About component

---
+++ src/tests/About.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import About from "../components/About";
+
+describe("About", () => {
+  it("renders correctly", () => {
+    const { asFragment } = render(<About />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("renders the correct title", () => {
+    render(<About />);
+    const title = screen.getByText(/camilla priest-stephens/i);
+
+    expect(title).toBeInTheDocument();
+  });
+
+  it("renders the correct image", () => {
+    render(<About />);
+
+    const logo = screen.getByAltText("laptop logo");
+    expect(logo).toBeInTheDocument();
+  });
+});
