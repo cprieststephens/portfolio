4da106f54d7e831241fb21d632ef93ac9515f3a3
Camilla Prist-Stephens <cprieststephens@googlemail.com>
2023-08-28 10:29:15 +0100

    Adjust layout of About section

--- src/components/About.js
+++ src/components/About.js
@@ -6,13 +6,13 @@ const About = () => {
   return (
     <div className="about">
       <img className="about__logo" src={logo} alt="laptop logo" />
-      <h1>Camilla Priest-Stephens</h1>
-      <h2>Frontend Developer</h2>
-      <p className="about__intro">
+      <h1 className="about__name">Camilla Priest-Stephens</h1>
+      <h2 className="about__job">Frontend Developer</h2>
+      <p className="about__text">
         I am a graduate of Command Shift bootcamp looking for a role as a
         Frontend Developer.
       </p>
-      <p className="about__tech">
+      <p className="about__text">
         I have experience of React, JavaScript, CSS, Jest and React Testing
         Library and am always open to learning new tech!
       </p>
